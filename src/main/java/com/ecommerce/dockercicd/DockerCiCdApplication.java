3878f0d079478adc63eea09fedaeb2affdf8f035
Ashraful Islam <ashraful01934207337@gmail.com>
2025-08-21 11:24:37 +0600

    Initial commit

---
+++ src/main/java/com/ecommerce/dockercicd/DockerCiCdApplication.java
@@ -0,0 +1,42 @@
+package com.ecommerce.dockercicd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerCiCdApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(DockerCiCdApplication.class);
+
+    @GetMapping("/hello")
+    public String hello() {
+        log.info("Received request for /hello endpoint");
+        return "Hello, Docker!";
+
+    }
+
+    @GetMapping("/docker")
+    public String docker() {
+        log.info("Received request for /docker endpoint");
+        return "Hello, Docker! From Docker Demo App";
+
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        log.info("Received request for /test endpoint");
+        return "Hello, Docker! From  tes App";
+
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerCiCdApplication.class, args);
+    }
+
+}
